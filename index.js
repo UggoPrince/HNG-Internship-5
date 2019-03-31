d62750ca9c781797322cb0f8d93346846f0d5af6
uggoprince <uggoprince@gmail.com>
2019-03-31 15:46:39 +0100

    made some changes for styling. Added styling for body tag

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 //
-document.write('My first commit in this repo for HNG Internship 5.');
+document.write('My first commit in this repo for HNG Internship 5.<br/><b>Secondly I added css for styling.<b/>');
